6c67feb5d1ba4cb30eb39075ca410c4afd2c5b01
William Carey <billcarey@mac.com>
2020-04-21 08:16:04 -0400

    Angle tangle working?

---
+++ angle-measurements.js
@@ -0,0 +1,91 @@
+// Drawing utilities
+
+class DraggableVertex {
+	constructor(canvas, x, y, freedom, fill, updateNotifier) {
+		this.ctx = canvas.getContext("2d");
+		canvas.addEventListener("mousedown", this.startDrag.bind(this))
+		canvas.addEventListener("mousemove", this.drag.bind(this))
+		canvas.addEventListener("mouseup", this.stopDrag.bind(this))
+		
+		this.updateNotifier = updateNotifier
+		
+		this.x = x;
+		this.y = y;
+		this.freedom = freedom; // Stringly typed: HORIZONTAL|VERTICAL|FREE
+		this.fill = fill;
+		
+		this.isDragging = false;
+		
+		// Stuff that doesn't change
+		this.RADIUS = 12
+	}
+
+	draw() {
+		this.ctx.fillStyle = this.fill;
+		this.ctx.beginPath();
+		this.ctx.arc(this.x, this.y, this.RADIUS - 2, 0, 2 * Math.PI, false); 
+		this.ctx.closePath();
+		this.ctx.fill();
+
+		this.ctx.beginPath();
+		this.ctx.strokeStyle = "#ccc";
+		this.ctx.arc(this.x, this.y, this.RADIUS - 1, 0, 2 * Math.PI, false); 
+		this.ctx.closePath();
+		this.ctx.stroke();
+
+		this.ctx.beginPath();
+		this.ctx.strokeStyle = "#aaa";
+		this.ctx.arc(this.x, this.y, this.RADIUS, 0, 2 * Math.PI, false); 
+		this.ctx.closePath();
+		this.ctx.stroke();
+	}
+	
+	startDrag(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		
+		var dx = e.offsetX - this.x
+		var dy = e.offsetY - this.y
+		
+		if (Math.sqrt(dx * dx + dy * dy) <= this.RADIUS) {
+			console.log("Hit!");
+			this.isDragging = true;
+			this.fill = "#6666CC";
+			
+			this.startX = this.x;
+			this.startY = this.y;
+		}
+	}
+	
+	drag(e) {
+		if (!this.isDragging) {
+			return;
+		}
+		
+        e.preventDefault();
+        e.stopPropagation();
+
+        var dx = e.offsetX - this.startX;
+        var dy = e.offsetY - this.startY;
+
+		if (this.freedom == "HORIZONTAL" || this.freedom == "FREE") {
+			this.x += dx;
+		}
+        if (this.freedom == "VERTICAL" || this.freedom == "FREE") {
+			this.y += dy;
+        }
+
+        // reset the starting mouse position for the next mousemove
+        this.startX = e.offsetX;
+        this.startY = e.offsetY;
+		
+		this.updateNotifier();
+    }
+	
+	stopDrag(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		this.isDragging = false;
+	}
+}
