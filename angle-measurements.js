f9448598961e65d666db05a1322af34f6e0c4306
William Carey <billcarey@mac.com>
2020-04-23 07:55:43 -0400

    Dorking around with radial vertex stuff. Copy pasta time.

--- angle-measurements.js
+++ angle-measurements.js
@@ -1,97 +1,96 @@
 // Drawing utilities
 
-class DraggableVertex {
-	constructor(canvas, x, y, freedom, updateNotifier) {
+function DraggableVertex(canvas, x, y, freedom, updateNotifier)
+{
+	this.ctx = canvas.getContext("2d");
+	canvas.addEventListener("mousedown", this.startDrag.bind(this))
+	canvas.addEventListener("mousemove", this.drag.bind(this))
+	canvas.addEventListener("mouseup", this.stopDrag.bind(this))
+	
+	this.updateNotifier = updateNotifier
+	
+	this.fill = "#3333CC";
+	
+	this.x = x;
+	this.y = y;
+	this.freedom = freedom; // Stringly typed: HORIZONTAL|VERTICAL|FREE
+	
+	this.isDragging = false;
+	
+	// Stuff that doesn't change
+	this.RADIUS = 12
+}
 
-		this.ctx = canvas.getContext("2d");
-		canvas.addEventListener("mousedown", this.startDrag.bind(this))
-		canvas.addEventListener("mousemove", this.drag.bind(this))
-		canvas.addEventListener("mouseup", this.stopDrag.bind(this))
+DraggableVertex.prototype.draw = function()
+{
+	this.ctx.fillStyle = this.fill;
+	this.ctx.beginPath();
+	this.ctx.arc(this.x, this.y, this.RADIUS - 2, 0, 2 * Math.PI, false); 
+	this.ctx.closePath();
+	this.ctx.fill();
+
+	this.ctx.beginPath();
+	this.ctx.strokeStyle = "#ccc";
+	this.ctx.arc(this.x, this.y, this.RADIUS - 1, 0, 2 * Math.PI, false); 
+	this.ctx.closePath();
+	this.ctx.stroke();
+
+	this.ctx.beginPath();
+	this.ctx.strokeStyle = "#aaa";
+	this.ctx.arc(this.x, this.y, this.RADIUS, 0, 2 * Math.PI, false); 
+	this.ctx.closePath();
+	this.ctx.stroke();
+}
+	
+DraggableVertex.prototype.startDrag = function(e)
+{
+	e.preventDefault();
+	e.stopPropagation();
+	
+	var dx = e.offsetX - this.x
+	var dy = e.offsetY - this.y
+	
+	if (Math.sqrt(dx * dx + dy * dy) <= this.RADIUS) {
+		this.isDragging = true;
+		this.fill = "#6666CC";
 		
-		this.updateNotifier = updateNotifier
-		
-		this.fill = "#3333CC";
-		
-		this.x = x;
-		this.y = y;
-		this.freedom = freedom; // Stringly typed: HORIZONTAL|VERTICAL|FREE
-		
-		this.isDragging = false;
-		
-		// Stuff that doesn't change
-		this.RADIUS = 12
+		this.startX = this.x;
+		this.startY = this.y;
 	}
-
-	draw() {
-		this.ctx.fillStyle = this.fill;
-		this.ctx.beginPath();
-		this.ctx.arc(this.x, this.y, this.RADIUS - 2, 0, 2 * Math.PI, false); 
-		this.ctx.closePath();
-		this.ctx.fill();
-
-		this.ctx.beginPath();
-		this.ctx.strokeStyle = "#ccc";
-		this.ctx.arc(this.x, this.y, this.RADIUS - 1, 0, 2 * Math.PI, false); 
-		this.ctx.closePath();
-		this.ctx.stroke();
-
-		this.ctx.beginPath();
-		this.ctx.strokeStyle = "#aaa";
-		this.ctx.arc(this.x, this.y, this.RADIUS, 0, 2 * Math.PI, false); 
-		this.ctx.closePath();
-		this.ctx.stroke();
+}
+	
+DraggableVertex.prototype.drag = function(e)
+{
+	if (!this.isDragging) {
+		return;
 	}
 	
-	startDrag(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		
-		var dx = e.offsetX - this.x
-		var dy = e.offsetY - this.y
-		
-		if (Math.sqrt(dx * dx + dy * dy) <= this.RADIUS) {
-			console.log("Hit!");
-			this.isDragging = true;
-			this.fill = "#6666CC";
-			
-			this.startX = this.x;
-			this.startY = this.y;
-		}
+    e.preventDefault();
+    e.stopPropagation();
+
+    var dx = e.offsetX - this.startX;
+    var dy = e.offsetY - this.startY;
+
+	if (this.freedom == "HORIZONTAL" || this.freedom == "FREE") {
+		this.x += dx;
 	}
-	
-	drag(e) {
-		if (!this.isDragging) {
-			return;
-		}
-		
-        e.preventDefault();
-        e.stopPropagation();
-
-        var dx = e.offsetX - this.startX;
-        var dy = e.offsetY - this.startY;
-
-		if (this.freedom == "HORIZONTAL" || this.freedom == "FREE") {
-			this.x += dx;
-		}
-        if (this.freedom == "VERTICAL" || this.freedom == "FREE") {
-			this.y += dy;
-        }
-		
-		if (this.freedom == "")
-
-        // reset the starting mouse position for the next mousemove
-        this.startX = e.offsetX;
-        this.startY = e.offsetY;
-		
-		this.updateNotifier();
+    if (this.freedom == "VERTICAL" || this.freedom == "FREE") {
+		this.y += dy;
     }
+
+    // reset the starting mouse position for the next mousemove
+    this.startX = this.x;
+    this.startY = this.y;
 	
-	stopDrag(e) {
-		e.preventDefault();
-		e.stopPropagation();
-		
-		this.fill = "#3333CC";
-		this.isDragging = false;
-		this.updateNotifier();
-	}
+	this.updateNotifier();
+}
+	
+DraggableVertex.prototype.stopDrag = function(e)
+{
+	e.preventDefault();
+	e.stopPropagation();
+	
+	this.fill = "#3333CC";
+	this.isDragging = false;
+	this.updateNotifier();
 }
